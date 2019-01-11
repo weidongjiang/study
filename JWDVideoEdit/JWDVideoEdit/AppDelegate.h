4e5488c53524203f3da58aef9d1d6e1f5d344bd8
yixiajwd <jiangweiodng@yixia.com>
2019-01-11 19:20:12 +0800

    补刀

---
+++ JWDVideoEdit/JWDVideoEdit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWDVideoEdit
+//
+//  Created by yixiajwd on 2018/11/23.
+//  Copyright © 2018 yixiajwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWDVideoEdit/JWDVideoEdit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWDVideoEdit
+//
+//  Created by yixiajwd on 2018/11/23.
+//  Copyright © 2018 yixiajwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
